4bee5bd263a2e5c5da4bbbea1e5b8fd24336d5d0
Francisco Hermida <483242+franciscohermida@users.noreply.github.com>
2025-01-25 21:36:15 +1100

    compatibility with nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
+  future: { compatibilityVersion: 4 },
 })
